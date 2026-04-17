0aa3f793bdda436957f678a3b8c611ad7f23e918
Theo Ruefli (RIT Student) <twr7348@rit.edu>
2026-04-17 12:50:08 -0400

    Added Get Character

--- server/controllers/Character.js
+++ server/controllers/Character.js
@@ -31,21 +31,80 @@ const createCharacter = async (req, res) => {
     }
 }
 
-const deleteDomo = async (req, res) => {
+const editCharacter = async (req, res) => {
+    const characterData = {
+        name: req.body.name,
+        age: req.body.age,
+        cheeseWheels: req.body.cheeseWheels,
+        owner: req.session.account._id,
+    };
+
+    try {
+        const newDomo = new Domo(domoData);
+        await newDomo.save();
+        //return res.json({ redirect: '/maker' });
+        return res.status(201).json({ name: newDomo.name, age: newDomo.age, cheeseWheels: newDomo.cheeseWheels });
+    } catch (err) {
+        console.log(err);
+        if (err.code === 11000) {
+            return res.status(400).json({ error: 'Domo already exists!' });
+        }
+        return res.status(500).json({ error: 'An error occured making domo!' });
+    }
+}
+
+const deleteCharacter = async (req, res) => {
     if(!req.body.name) {
         return res.status(400).json({ error: 'Name is required!' });
     }
 
     try {
-        //const item = await Domo.deleteOne(req.body.name);
-        const goner = await Domo.findOneAndDelete({ name: req.body.name });
+        const theChosenOne = await Character.findOneAndDelete({ name: req.body.name });
 
-        if(!goner) {
-            return res.status(400).json({error: `Domo ${req.body.name} not found!`});
+        if(!theChosenOne) {
+            return res.status(400).json({error: `Character ${req.body.name} not found!`});
         }
-        return res.status(200).json({ message: `Domo ${req.body.name} deleted!` });
+        return res.status(200).json({ message: `Character ${req.body.name} deleted!` });
     } catch (err) {
-        return res.status(500).json({ error: 'An error occured deleting domo!' });
+        console.log(err);
+        return res.status(500).json({ error: 'An error occured deleting Character!' });
+    }
+}
+
+const getCharacterByName = async (req, res) => {
+    if(!req.body.name) {
+        return res.status(400).json({ error: 'Name is required!' });
+    }
+
+    try {
+        // can probably change from doc to something else later
+        const docs = await Character.find(req.body.name).lean().exec();
+
+        return res.json({character: docs});
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: 'Error retrieving character!'});
+    }
+}
+
+const getCharactersByUser = async (req, res) => {
+    const query = { owner: req.session.account._id };
+    getCharacters(req, res, query);
+}
+
+const getCharactersByCampaign = async (req, res) => {
+    const query = { campaignID: req.body.campaignID };
+    getCharacters(req, res, query);
+}
+
+const getCharacters = async (req, res, query) => {
+    try {
+        const docs = await Character.find(query).select('name').lean().exec();
+
+        return res.json({ characters: docs });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: 'Error retrieving characters!' });
     }
 }
 
